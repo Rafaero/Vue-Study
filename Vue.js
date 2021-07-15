73a4790d26021cacecf060d6bcf91e901081829b
Rafael de Paiva <rafaelpaivavitor@hotmail.com>
2021-07-15 14:56:06 -0300

    Update Vue.js

--- Vue.js
+++ Vue.js
@@ -1,1 +1,21 @@
+# **Instalação**
 
+O Vue.js foi desenvolvido para ser adotado de forma incremental. Isso significa que pode ser integrado em um projeto de várias maneiras, dependendo dos requisitos.
+
+Existem quatro maneiras principais de adicionar Vue.js a um projeto:
+
+1. Importe-o como um **[pacote CDN](https://v3.vuejs.org/guide/installation.html#cdn)**
+
+    na página
+
+2. Baixe os arquivos JavaScript e **[hospede-os você mesmo](https://v3.vuejs.org/guide/installation.html#download-and-self-host)**
+3. Instale-o usando **[npm](https://v3.vuejs.org/guide/installation.html#npm)**
+4. Use a **[CLI](https://v3.vuejs.org/guide/installation.html#cli)**
+
+    oficial para estruturar um projeto, que fornece configurações de compilação com baterias para um fluxo de trabalho de front-end moderno (por exemplo, hot-reload, lint-on-save e muito mais)
+
+    ## **O que é Vue.js?**
+
+    Vue (pronuncia-se / vjuː /, como **view** ) é uma **estrutura progressiva** para construir interfaces de usuário. Ao contrário de outras estruturas monolíticas, o Vue foi projetado desde o início para ser adotado de forma incremental. A biblioteca central concentra-se apenas na camada de visualização e é fácil de selecionar e integrar com outras bibliotecas ou projetos existentes. Por outro lado, o Vue também é perfeitamente capaz de fornecer aplicativos sofisticados de página única quando usado em combinação com **[ferramentas modernas](https://v3.vuejs.org/guide/single-file-component.html)** e **[bibliotecas de suporte (abre uma nova janela)](https://github.com/vuejs/awesome-vue#components--libraries)**.
+
+    Se você gostaria de saber mais sobre o Vue antes de mergulhar, **[criamos um vídeo que](https://v3.vuejs.org/guide/introduction.html#)** mostra os princípios básicos e um projeto de amostra.
